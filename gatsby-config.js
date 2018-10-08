5ef0610cfc324b816e4d868147dce586e4f289bb
Connor Wilson <connor.wilson@tulip.com>
2018-10-08 17:52:45 -0400

    fix config to remove favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-contentful-typescript',
+        name: 'cwlsn',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#333333',
+        theme_color: '#333333',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
